5e6029b0f892ee0cb17289ebf500b5b76cb7ed67
SonTrungTo <son.trung.to@gmail.com>
2020-04-16 19:23:22 +0300

    Add some basic jqueries for future ref...
    - Bootstrap, CSS Flexbox, CSS grid nearly finished

---
+++ Bootstrap_Flexbox/jquery.js
@@ -0,0 +1,40 @@
+
+// Disable form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+// Add the following code if you want the name of the file appear on select
+$(".custom-file-input").on("change", function() {
+  var fileName = $(this).val().split("\\").pop();
+  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
+
+// Tooltip activation
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Popover activation
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();
+});
+
+// Toast activation
+$(document).ready(function(){
+  $('.toast').toast('show');
+});
