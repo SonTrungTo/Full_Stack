88ed21b550aa8ce6333a49e43304288721e44893
SonTrungTo <son.trung.to@gmail.com>
2020-04-23 16:49:03 +0300

    some examples (difficult ones)

--- EloquentJS/example.js
+++ EloquentJS/example.js
@@ -1,9 +1,1 @@
-  function factorial(n) {
-    if (n==0) {
-      return 1;
-    } else {
-      return factorial(n-1)*n ;
-    }
-  }
-
-  console.log(factorial(8));
+console.log(`${"(1 x 3)"} + 5`);
---
+++ EloquentJS/findSolution.js
@@ -0,0 +1,15 @@
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target)
+      return history;
+    if (current > target)
+      return null;
+    if (current < target) {
+      return find(current + 5,`(${history} + 5)`) ||
+              find(current * 3, `(${history} x 3)`);
+    }
+  }
+  return  find(1,"1");
+}
+
+console.log(findSolution(24));
