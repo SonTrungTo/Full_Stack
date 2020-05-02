c24ad5073354e04f958e2b559ec25e29926ec0fb
SonTrungTo <son.trung.to@gmail.com>
2020-05-02 23:39:19 +0300

    symbol codes added.

--- EloquentJS/chapter6.js
+++ EloquentJS/chapter6.js
@@ -4,3 +4,20 @@ function normalize() {
 }
 
 normalize.call({coord: [0, 2, 3], length: 5});
+let obj = {coord: [0, 2, 3], length: 5};
+console.log(Object.getPrototypeOf(obj) == Object.prototype);
+console.log(Object.prototype.toString.call([1,2]));
+console.log(obj.toString());
+
+// Symbol: It's possible for multiple interfaces to use the same property for different purposes.
+let sym = Symbol("toString");
+console.log(sym);
+console.log(sym == "toString");
+console.log(String(obj));
+let toStringProperty = Symbol("toString");
+Array.prototype[toStringProperty] = function () {
+  return `${this.length} cm of blue yarn`;
+}
+
+console.log([1,2].toString());
+console.log([1,2][toStringProperty]());
