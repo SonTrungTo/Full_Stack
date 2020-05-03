23e2c1dad86b4bf2890f21c16eeafdaceec5fd5e
SonTrungTo <son.trung.to@gmail.com>
2020-05-03 09:17:16 +0300

    some notes for polymorphism, iterator interface

--- EloquentJS/chapter6.js
+++ EloquentJS/chapter6.js
@@ -21,3 +21,22 @@ Array.prototype[toStringProperty] = function () {
 
 console.log([1,2].toString());
 console.log([1,2][toStringProperty]());
+
+// Symbol.iterator as an interface (method) that is used by for/of, requiring the object
+// received to be iterable. It returns the second interface, interator with next() =>
+// an object with value and done property.
+let okIterator = "OK"[Symbol.iterator]();
+console.log(okIterator.next());
+console.log(okIterator.next());
+console.log(okIterator.next());
+
+// Let us now build an iterable data structure (matrix class)
+// As long as the instance of class communicates with for/of via appropriate interface
+// we can iterate the data structure.
+class Matrix {
+  constructor(width, height, element = (x,y) => undefined) {
+    this.width   = width;
+    this.height  = height;
+    this.content = [];
+  }
+}
