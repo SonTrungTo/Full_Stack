cda24ccc0f221f07838d123813e59bdbe5ae32c2
SonTrungTo <son.trung.to@gmail.com>
2020-05-03 19:56:44 +0300

    Iterable data structure finished!

--- EloquentJS/chapter6.js
+++ EloquentJS/chapter6.js
@@ -38,5 +38,54 @@ class Matrix {
     this.width   = width;
     this.height  = height;
     this.content = [];
+
+    for (let y = 0; y < height ; ++y) {
+      for (let x = 0; x < width ; ++x) {
+        this.content[y * width + x] = element(x,y);
+      }
+    }
   }
+  get(x,y) {
+    return this.content[y * this.width + x];
+  }
+  set(x,y,value) {
+    this.content[y * this.width + x] = value;
+  }
+}
+
+// Now we build the Matrix Iterator, an interface to iterate the matrix
+// It needs to keep track of its coordinator and its value.
+class MatrixIterator {
+  constructor(matrix) {
+    this.x      = 0;
+    this.y      = 0;
+    this.matrix = matrix;
+  }
+
+  next() {
+    if (this.y == this.matrix.height) return {done: true};
+    let value = {
+      x: this.x,
+      y: this.y,
+      value: this.matrix.get(this.x , this.y)
+    };
+    this.x++;
+    if (this.x == this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+
+    return {value, done: false};
+  }
+}
+
+// Now, load it to Matrix.prototype
+Matrix.prototype[Symbol.iterator] = function() {
+  return new MatrixIterator(this);
+}
+
+// Let's loop!
+let matrix = new Matrix(2,2,(x,y) => `value ${x}, ${y}`)
+for (let {x,y,value} of matrix) {
+  console.log(x,y,value);
 }
