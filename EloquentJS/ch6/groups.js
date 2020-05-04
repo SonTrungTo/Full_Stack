f8d81db804e74ba2c3af233fc88b564c470b388a
SonTrungTo <son.trung.to@gmail.com>
2020-05-04 13:42:43 +0300

    ex6.2 finished!

--- EloquentJS/ch6/groups.js
+++ EloquentJS/ch6/groups.js
@@ -4,16 +4,39 @@ set.add(3);
 set.add(0);
 console.log(set.has(1));
 console.log(set);
+// Group class
 class Group {
   constructor() {
     this.member = [];
   }
   add(value) {
-    if (!this.member.includes(value)) return this.member.push(value);
+    if (this.member.indexOf(value) == -1) this.member.push(value);
+  }
+  delete(value) {
+    this.member = this.member.filter(n => n != value);
+  }
+  has(value) {
+    return this.member.indexOf(value) == -1 ? false : true;
+  }
+  static from(object) {
+    let newGroup = new Group();
+    for (let element of object) {
+      newGroup.member.push(element);
+    }
+    return newGroup;
   }
 }
 
 let group = new Group();
 group.add(1);
+group.add(2);
+group.add(3);
 group.add(1);
+group.add(5);
+group.delete(2);
+group.delete(1);
+console.log(group);
+console.log(group.has(4));
+let group2 = Group.from([1,3,5,7,9,11]);
+console.log(group2);
 console.log(group);
