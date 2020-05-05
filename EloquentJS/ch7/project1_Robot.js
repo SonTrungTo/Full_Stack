ae23c2d5ef183fea1a551620d6d122d8ba5cc5eb
SonTrungTo <son.trung.to@gmail.com>
2020-05-05 23:03:34 +0300

    roadGraph built!

--- EloquentJS/ch7/project1_Robot.js
+++ EloquentJS/ch7/project1_Robot.js
@@ -1,2 +1,30 @@
 // Roads form a graph
-const roads = ["Cabin-Alice's House", "Alice's House-Bob's House"];
+const roads = ["Cabin-Alice's House", "Alice's House-Bob's House",
+               "Alice's House-Post Office", "Bob's House-Town Hall",
+               "Dick's House-Erina's House", "Dick's House-Town Hall",
+               "Erina's House-George's House", "George's House-Farm",
+               "George's House-Shop", "Farm-Market",
+               "Market-Post Office", "Market-Shop",
+               "Market-Town Hall", "Town Hall-Shop"];
+// Convert to a data structure that tells what places can be reached from a certain node.
+function buildGraph(edges) {
+  let graph = Object.create(null);
+
+  function addEdges(from, to) {
+    if (graph[from] == undefined) {
+        graph[from] = [to];
+    } else {
+        graph[from].push(to);
+    }
+  }
+
+  for (let [from, to] of edges.map(s => s.split("-"))) {
+    addEdges(from, to);
+    addEdges(to, from);
+  }
+
+  return graph;
+}
+
+let roadGraph = buildGraph(roads);
+console.log(roadGraph);
