2c27cd951392d03bdecee38770b696072d4fcd7a
SonTrungTo <son.trung.to@gmail.com>
2020-05-11 23:58:07 +0300

    ex 7.3 finished! ch7 done! Revisit if have time!

--- EloquentJS/ch7/README.md
+++ EloquentJS/ch7/README.md
@@ -5,4 +5,11 @@ project1_Robot contains exercise 7.1, 7.2
 7.2 (Robot Efficiency): One of the best exercises in the book.
 I cannot solve this one completely.
 
-7.3 (Persistent Group)
+7.3 (Persistent Group): I cannot solve this completely.
+
+"Why do you need only one PGroup.empty value, rather than having a function
+that creates a new, empty map every time?"
+
+Because PGroup.empty can give different instances of class PGroup without needing
+any properties, methods or values from the class itself. Hence, having a function
+is not necessary.
--- EloquentJS/ch7/persistentGroup.js
+++ EloquentJS/ch7/persistentGroup.js
@@ -1,15 +1,35 @@
 class PGroup {
-  constructor(value) {
-    this.member = value;
+  constructor(members) {
+    this.member = members;
   }
 
   add(value) {
+    if (this.has(value)) return  new PGroup(this.member); // or *this* is ok since new instance every time.
     return new PGroup(this.member.concat(value));
   }
-}
 
-let testPGroup = new PGroup();
+  delete(value) {
+    return new PGroup(this.member.filter(m => m !== value));
+  }
+
+  has(value) {
+    if (this.member.some(m => m == value)) {
+      return true;
+    }
+    return false;
+  }
+}
+PGroup.empty = new PGroup([]);
+
+let testPGroup = PGroup.empty;
 console.log(testPGroup);
 let newPGroup = testPGroup.add(1);
-console.log(newPGroup);
+let newPGroup2 = newPGroup.add(1);
+let newPGroup3 = newPGroup2.add(2);
+let newPGroup4 = newPGroup3.add("S");
+let newPGroup5 = newPGroup4.delete(2);
+console.log(newPGroup4);
+console.log(newPGroup3);
+console.log(newPGroup5);
+console.log(newPGroup4);
 console.log(testPGroup);
