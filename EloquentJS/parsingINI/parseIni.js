32805f1b298d3ccb88f0f3a79d899bb53da59d26
SonTrungTo <son.trung.to@gmail.com>
2020-05-18 00:57:57 +0300

    parseINI finished!

--- EloquentJS/parsingINI/parseIni.js
+++ EloquentJS/parsingINI/parseIni.js
@@ -7,11 +7,24 @@ function parseINI(string) {
   // Start with an object that holds the top-level fields
   let result = {};
   let section = result;
+  let match;
+
+  string.split(/\r?\n/).forEach(line => {
+    if (match = line.match(/^(\w+) = (.*)$/)) {
+      section[match[1]] = match[2];
+    } else if (match = line.match(/^\[(.*)\]$/)) {
+      section = result[match[1]] = {};
+    } else if (!line.match(/^\s*(;.*)?$/)) {
+      throw new Error("Line '" + line + "' is invalid!");
+    }
+  });
+
+  return result;
 }
 
 try {
   let data = fs.readFileSync("data.ini", "utf-8");
-  console.log(data);
+  console.log(parseINI(data));
 } catch (e) {
   console.log("Error:", e);
 }
