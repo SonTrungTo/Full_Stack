c18c3c07c02efa15d10adb9b2648c94ee32ecada
SonTrungTo <son.trung.to@gmail.com>
2020-05-18 11:24:50 +0300

    ex6.2 in progess...

---
+++ EloquentJS/ch9/quotingStyle.js
@@ -0,0 +1,8 @@
+// Exercise 9.2 begins here....
+console.log(/\W''\W/.test("aren't"));
+
+// need to test
+\W'.*?'
+
+'i'm the cook'... 'ala'. I ain't go; the chef said, 'fine,'
+'ala,' asd ' asdf,' asdf '???'. He said, 'no you traitor!'.
