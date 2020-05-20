4cf5f50f4b2fe02c635a5be1013b0cdb7b50aad9
SonTrungTo <son.trung.to@gmail.com>
2020-05-20 18:33:06 +0300

    add some testing on Date

--- EloquentJS/chapter10.js
+++ EloquentJS/chapter10.js
@@ -1,1 +1,8 @@
 // Chapter 10: Modules incoming....
+// Testing Date object
+let date = new Date();
+console.log(date);
+console.log(date.getDay());
+console.log(date.getDate());
+console.log(date.getFullYear());
+console.log(date.getMonth());
