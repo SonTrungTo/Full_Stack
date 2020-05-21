87388bfb9add3f8b9ae628e960e117389e147728
SonTrungTo <son.trung.to@gmail.com>
2020-05-21 10:28:05 +0300

    CommonJS writing examples...

--- EloquentJS/chapter10.js
+++ EloquentJS/chapter10.js
@@ -1,12 +1,20 @@
-// Chapter 10: Modules incoming....
-// Testing Date object
-let date = new Date();
-console.log(date);
-console.log(date.getDay());
-console.log(date.getDate());
-console.log(date.getFullYear());
-console.log(date.getMonth());
+// Before 2015, there was no module system. So we have to use functions to create
+// local scope and objects as the module's interfaces.
+// Consequently, there is no dependencies declaration. The interfaces are declared
+// in global scope, so is any dependency. OUTDATED
+const weekDay = function () {
+  let names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+               "Friday", "Saturday"];
 
-const {days, months} = require("date-names");
-console.log(days);
-console.log(months);
+  return {
+    name(number) {return names[number];},
+    number(name) {return names.indexOf(name);}
+  };
+}();
+
+console.log(weekDay.name(weekDay.number("Thursday")));
+
+// So we need to run string as part of the code. One such is to use Function.
+// Fortunately, CommonJS module loads the module, wraps it in a function and
+// exports its interface. (with NPM for massive scale development.)
+const ordinal = require('ordinal');
