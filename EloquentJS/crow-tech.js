c84a52d3069663aa1433c8c82a133cf6eb6b4fd5
SonTrungTo <son.trung.to@gmail.com>
2020-05-28 19:02:18 +0300

    crow-tech.js writing...

---
+++ EloquentJS/crow-tech.js
@@ -0,0 +1,7 @@
+(function() {
+  const connections = [
+    "Church Tower-Sportsgrounds", "Church Tower-Big Maple", "Big Maple-Sportsgrounds",
+    "Big Maple-Woods", "Big Maple-Fabienne's Garden", "Fabienne's Garden-Woods",
+    "Fabienne's Garden-Cow Pasture", "Cow Pasture-Big Oak", "Big Oak-Butcher Shop"
+  ];
+})();
