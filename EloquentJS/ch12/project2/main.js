11384eb6c826f329848400dccc0dc733c069fa0f
SonTrungTo <son.trung.to@gmail.com>
2020-06-08 23:00:37 +0300

    topScope written! now finishing with running program

--- EloquentJS/ch12/project2/main.js
+++ EloquentJS/ch12/project2/main.js
@@ -1,5 +1,6 @@
 // Egg, the programming language in JavaScript
 const {parse} = require('./parser');
 const {evaluate} = require('./evaluator');
+const {}
 
 console.log(parse(`+(a, 10)`));
---
+++ EloquentJS/ch12/project2/topScope.js
@@ -0,0 +1,13 @@
+let topScope = Object.create(null);
+
+for (let op of ["+", "-", "*", "/", "%", "==", "<", ">"]) {
+  topScope[op] = Function(`a, b`, `return a ${op} b;`);
+}
+
+// Since you don't need to evaluate anything!
+topScope.print = function(...args) {
+  console.log(...args);
+  return false;
+}
+
+exports.topScope = topScope;
