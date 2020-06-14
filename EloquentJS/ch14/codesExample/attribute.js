6b86f1524a079b655a14d59ded8270762d19bd88
SonTrungTo <son.trung.to@gmail.com>
2020-06-14 17:29:00 +0300

    layout in process...

---
+++ EloquentJS/ch14/codesExample/attribute.js
@@ -0,0 +1,6 @@
+let paras = document.body.getElementsByTagName('p');
+for (let para of Array.from(paras)) {
+  if (para.getAttribute("data-classified") == "secret") {
+    para.remove();
+  }
+}
---
+++ EloquentJS/ch14/codesExample/layout.js
@@ -0,0 +1,6 @@
+let para = document.getElementById('layout');
+console.log("offsetWidth:", para.offsetWidth);
+console.log("clientWidth:", para.clientWidth);
+console.log("getBoundingClientRect:", para.getBoundingClientRect());
+console.log("offsetHeight:", para.offsetHeight);
+console.log("clientHeight:", para.clientHeight);
