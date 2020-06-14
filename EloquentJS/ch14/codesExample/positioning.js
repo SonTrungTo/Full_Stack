b55b3db62913780a5dc4d243855a7aee7cc55909
SonTrungTo <son.trung.to@gmail.com>
2020-06-14 20:19:40 +0300

    positioning finished! now solving exercises...

--- EloquentJS/ch14/codesExample/positioning.js
+++ EloquentJS/ch14/codesExample/positioning.js
@@ -1,2 +1,11 @@
 let book = document.querySelector("#animation");
 let angle = Math.PI / 2;
+function animate(time, lastTime) {
+  if (lastTime != null) {
+    angle += (time - lastTime) * 0.001;
+  }
+  book.style.top  = (Math.sin(angle) * 20)  + "px";
+  book.style.left = (Math.cos(angle) * 200) + "px";
+  requestAnimationFrame(newTime => animate(newTime, time));
+}
+requestAnimationFrame(animate);
