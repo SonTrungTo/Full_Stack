564dfd3f6958579a104a05d612e1ab28fd73ffd6
SonTrungTo <son.trung.to@gmail.com>
2020-06-15 11:17:46 +0300

    build a Table, pt 2: Finished table head

--- EloquentJS/ch14/buildATable.js
+++ EloquentJS/ch14/buildATable.js
@@ -1,6 +1,18 @@
 function buildTable(data) {
   let table = document.createElement('table');
-    
+  // Creating column head names for the table
+  if (document.querySelectorAll("#one > table th").length != 3) {
+    let row = document.createElement("tr");
+    for (let colName of Object.keys(data[0])) {
+      let head = document.createElement("th");
+      let text = document.createTextNode(colName);
+      head.appendChild(text);
+      row.appendChild(head); // Note: head.appendChild(text) is a method and returns nodeText, not head node.
+    }
+    table.appendChild(row);
+  }
+  // Now creating table data
+  
   return table;
 }
 
