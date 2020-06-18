eb8f2586cc01e8c07d4153a96c516e241d8a6d1e
SonTrungTo <son.trung.to@gmail.com>
2020-06-19 00:02:54 +0300

    ReactTutorial finished! (basics!)
    - A Tic-Tac-Toe game that leaves rooms for improvements.
    (see the tutorial for more suggestions,
    - Will come back if I have time

--- ReactTutorial/my-app/src/index.js
+++ ReactTutorial/my-app/src/index.js
@@ -55,11 +55,12 @@ class Game extends React.Component {
         squares: Array(9).fill(null)
       }],
       xIsNext: true,
+      stepNumber: 0,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (squares[i] || calculateTheWinner(squares)) {
@@ -69,12 +70,20 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{squares: squares}]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length, // Reflect the move displayed to the user.
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner  = calculateTheWinner(current.squares);
 
     const moves = history.map((step, move) => {
