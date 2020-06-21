c94b3a4e7ed59140596af4183caba576446f8ef1
SonTrungTo <son.trung.to@gmail.com>
2020-06-21 04:08:43 +0300

    touchevent added, ver2!

--- EloquentJS/ch15/codesExample/touch.js
+++ EloquentJS/ch15/codesExample/touch.js
@@ -8,10 +8,18 @@ function update(event) {
     dot.style.left = (pageX - 50) + "px";
     dot.style.top  = (pageY - 50) + "px";
     document.body.appendChild(dot);
-    event.preventDefault();
   }
 }
 
-window.addEventListener("touchstart", update);
-window.addEventListener("touchmove", update);
-window.addEventListener("touchend", update);
+window.addEventListener("touchstart", event => {
+  update(event);
+  event.preventDefault(); // Prevent scrolling and mouse event firing up.
+});
+window.addEventListener("touchmove", event => {
+  update(event);
+  event.preventDefault();
+});
+window.addEventListener("touchend", event => {
+  update(event);
+  event.preventDefault();
+});
