ed4a51d61fefcc6d715fe4f366710a593d8e8416
SonTrungTo <son.trung.to@gmail.com>
2020-06-22 15:11:55 +0300

    balloon finished!

--- EloquentJS/ch15/balloon.js
+++ EloquentJS/ch15/balloon.js
@@ -1,5 +1,19 @@
-console.log("🎈");
-console.log("💥");
-window.addEventListener("keydown", event => {
-  
-});
+let balloon = document.querySelector(".balloon");
+let size = 12;
+
+window.addEventListener("keydown", adjustBalloon);
+
+function adjustBalloon(event) {
+  if (event.key == "ArrowUp") {
+    size *= (1 + 0.1);
+    balloon.style.fontSize = size + "px";
+  } else if (event.key == "ArrowDown") {
+    size *= (1 - 0.1);
+    balloon.style.fontSize = size + "px";
+  }
+  if (size > 100) {
+    balloon.textContent = "💥";
+    window.removeEventListener("keydown", adjustBalloon);
+  }
+  event.preventDefault();
+}
