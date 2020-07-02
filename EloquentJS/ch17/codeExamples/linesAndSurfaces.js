d9962537d9a0791b24fd7fa3c34d0b4b77678c26
SonTrungTo <son.trung.to@gmail.com>
2020-07-03 00:05:29 +0300

    linesAndSurfaces.js added...
    for lineWidth, strokeRect, strokeStyle methods

---
+++ EloquentJS/ch17/codeExamples/linesAndSurfaces.js
@@ -0,0 +1,6 @@
+let cx = document.querySelector("#canvas2").getContext("2d");
+cx.strokeStyle = "blue";
+cx.strokeRect(5, 5, 50, 50);
+cx.lineWidth = 5;
+cx.strokeRect(120, 5, 50, 50);
+// default: 300px, 150px
