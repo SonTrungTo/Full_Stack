01cd455e7e3535eb96f7cde12bc1501aa685d327
SonTrungTo <son.trung.to@gmail.com>
2020-07-04 13:30:22 +0300

    CanvasDisplay still in progress...

--- EloquentJS/ch16/drawing/CANVASDisplay.js
+++ EloquentJS/ch16/drawing/CANVASDisplay.js
@@ -1,5 +1,8 @@
+const scale = 20;
+
 class CanvasDisplay {
   constructor(parent, level) {
-    
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = Math.min();
   }
 }
--- EloquentJS/ch17/codeExamples/canvas.js
+++ EloquentJS/ch17/codeExamples/canvas.js
@@ -1,4 +1,6 @@
 let canvas  = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 context.fillStyle = "red";
-context.fillRect(10, 10, 120, 60); 
+context.fillRect(10, 10, 120, 60);
+context.fillStyle = "pink";
+context.fillRect(10, 10, 120, 60);
