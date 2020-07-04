8a08f0be97cb85e481a7f31a0e5e83ea14e7ae5e
SonTrungTo <son.trung.to@gmail.com>
2020-07-04 16:27:23 +0300

    constructor is written. now doing it for updateViewPort

--- EloquentJS/ch16/drawing/CANVASDisplay.js
+++ EloquentJS/ch16/drawing/CANVASDisplay.js
@@ -3,6 +3,35 @@ const scale = 20;
 class CanvasDisplay {
   constructor(parent, level) {
     this.canvas = document.createElement("canvas");
-    this.canvas.width = Math.min();
+    this.canvas.width  = Math.min(600, level.width  * scale);
+    this.canvas.height = Math.min(450, level.height * scale);
+    parent.appendChild(this.canvas);
+    this.cx = this.canvas.getContext("2d");
+
+    this.flipPlayer = false;
+
+    this.viewport = {
+      top:    0,
+      left:   0,
+      width:  this.canvas.width  / scale,
+      height: this.canvas.height / scale
+    };
+  }
+
+  syncState(state) {
+    this.updateViewPort(state);
+    this.clearDisplay(state.status);
+    this.drawBackground(state.level);
+    this.drawActors(state.actors);
+  }
+
+  updateViewPort(state) {
+    let view   = this.viewport;
+    let margin = view.width / 3;
+    let player = state.player();
+    let center;
+
+  clear() {
+    this.canvas.remove();
   }
 }
