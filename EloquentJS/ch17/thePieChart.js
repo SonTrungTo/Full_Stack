72cbb14583b67fba0d18f5c5bfe96a60fd0cd814
SonTrungTo <son.trung.to@gmail.com>
2020-07-07 17:25:45 +0300

    Pie chart drawn! Now labelling...

--- EloquentJS/ch17/thePieChart.js
+++ EloquentJS/ch17/thePieChart.js
@@ -8,8 +8,23 @@ const results = [
 
 function createPieChart(results) {
   let canvas = document.createElement("canvas");
-  canvas.width  = 200;
-  canvas.height = 150;
+  canvas.width  = 300;
+  canvas.height = 300;
   pie.appendChild(canvas);
-  let cx = canvas.getContext("2d"); 
+  let cx = canvas.getContext("2d");
+
+  let total = results.reduce((sum, {count}) => sum + count, 0);
+  let startAngle = - Math.PI / 2;
+  for (let result of results) {
+    cx.beginPath();
+    let endAngle = 2 * Math.PI / total * result.count;
+    cx.arc(150, 150, 100, startAngle, startAngle + endAngle);
+    startAngle += endAngle;
+    cx.lineTo(150, 150);
+
+    cx.fillStyle = result.color;
+    cx.fill();
+  }
 }
+
+createPieChart(results);
