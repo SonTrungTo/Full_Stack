1ffb7a8618df3d9e124c280e3028675933ab030a
SonTrungTo <son.trung.to@gmail.com>
2020-07-07 04:58:19 +0300

    rewriting the pie chart...

--- EloquentJS/ch17/README.md
+++ EloquentJS/ch17/README.md
@@ -5,3 +5,6 @@ Drawing:
 1. A zigzagging line
 1. A spiral made up of 100 straight line segments
 1. A yellow star
+
+# thePieChart.js (The Pie Chart)
+Putting label on each pie of the chart.
---
+++ EloquentJS/ch17/thePieChart.js
@@ -0,0 +1,11 @@
+let pie = document.getElementById('pie');
+const results = [
+  {name: "Satisfied",   count: 1043, color: "lightblue"},
+  {name: "Neutral",     count: 563,  color: "lightgreen"},
+  {name: "Unsatisfied", count: 510,  color: "pink"},
+  {name: "No comment",  count: 175,  color: "silver"}
+];
+
+function createPieChart(results) {
+  let canvas;
+}
