85d2beda0e13b616e3e078c77f52d2a30c8d831c
SonTrungTo <son.trung.to@gmail.com>
2020-07-07 21:45:03 +0300

    Bouncing Ball, updated, ver2

--- EloquentJS/ch17/aBouncingBall.js
+++ EloquentJS/ch17/aBouncingBall.js
@@ -1,4 +1,18 @@
-// A bouncing ball
-function frame(time) {
-  
+let ball = document.getElementById('ball');
+
+function runAnimation(frameFunc) {
+  let lastTime = null;
+  function frame(time) {
+    if (lastTime != null) {
+      let timeStep = Math.min(time - lastTime, 100) / 1000;
+      if (frameFunc(timeStep) === false) return;
+    }
+    lastTime = time;
+    requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+}
+
+function updateFrame(time) {
+  let canvas = document.createElement("canvas");
 }
