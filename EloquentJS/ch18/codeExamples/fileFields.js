7d174c06279c24ab5b4e1def69006665a388d28b
SonTrungTo <son.trung.to@gmail.com>
2020-07-11 06:58:46 +0300

    fileFields, part I done!

---
+++ EloquentJS/ch18/codeExamples/fileFields.js
@@ -0,0 +1,9 @@
+let input = document.querySelector("#file");
+
+// input.addEventListener("change", () => {
+//   if (input.files.length > 0) {
+//     let file = input.files[0];
+//     console.log(`You chose ${file.name}`);
+//     if(file.type) console.log("It has type", file.type);
+//   }
+// });
