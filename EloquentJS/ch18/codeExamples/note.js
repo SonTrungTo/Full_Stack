afbddb6db7de5785355dff241c80b58ba50efccb
SonTrungTo <son.trung.to@gmail.com>
2020-07-12 00:39:29 +0300

    notes.html updated, ver 2

--- EloquentJS/ch18/codeExamples/note.js
+++ EloquentJS/ch18/codeExamples/note.js
@@ -1,1 +1,17 @@
 let list = document.querySelector("select");
+let note = document.querySelector("textarea");
+
+let state;
+function setState(newState) {
+  let list.textContent = "";
+  for (let name of Object.keys(newState.notes)) {
+    let option = document.createElement("option");
+    option.textContent = name;
+    if (newState.selected == name) option.selected = true;
+    list.appendChild(option);
+  }
+  note.value;
+
+  localStorage.setItem("Notes", JSON.stringify(newState));
+  state = newState;
+}
