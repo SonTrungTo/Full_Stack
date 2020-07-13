8dd91048d89062f3c9b1aaec2c845901673417a5
SonTrungTo <son.trung.to@gmail.com>
2020-07-13 21:49:14 +0300

    JSWorkbench finished! YAYYY!

--- EloquentJS/ch18/javascriptWorkbench.js
+++ EloquentJS/ch18/javascriptWorkbench.js
@@ -1,13 +1,24 @@
-let codesBody = document.querySelector("textarea");
+let textarea = document.querySelector("textarea");
 let runCodes  = document.querySelector("#runCodes");
 let helpText  = document.querySelector("#help");
 
 runCodes.addEventListener("click", () => {
+  helpText.textContent = "";
+  let codesBody = textarea.value;
+  console.log = function(value) {
+    let valueNode = document.createTextNode(String(value));
+    let para      = document.createElement("p");
+    para.appendChild(valueNode);
+    helpText.appendChild(para);
+  }
+
   try {
-    let result = Function("", codesBody.value);
-    let output = result();
-    helpText.textContent = String(output);
+    let run = Function("", codesBody);
+    run();
   } catch (e) {
-    helpText.textContent = String(e);
+    let para = document.createElement("p");
+    let errorMessage = document.createTextNode("Error: " + String(e));
+    para.appendChild(errorMessage);
+    helpText.appendChild(para);
   }
 });
