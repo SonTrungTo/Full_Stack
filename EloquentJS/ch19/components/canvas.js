0c75fbcbb9de0ed8cfb5e04226ec98026ed8a57d
SonTrungTo <son.trung.to@gmail.com>
2020-07-17 18:30:48 +0300

    class PictureCanvas done!

--- EloquentJS/ch19/components/canvas.js
+++ EloquentJS/ch19/components/canvas.js
@@ -1,10 +1,17 @@
+const scale = 10;
+
 class PictureCanvas {
   constructor(picture, pointerDown) {
-    this.dom;
+    this.dom = elt("canvas", {
+      onmousedown: event => this.mouse(event, pointerDown),
+      ontouchstart: event => this.touch(event, pointerDown)
+    });
     this.syncState(picture);
   }
 
   syncState(picture) {
-    
+    if (this.picture == picture) return;
+    this.picture = picture;
+    drawPicture(this.picture, this.dom, scale)
   }
 }
