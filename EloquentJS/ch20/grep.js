a1478a61287d3e10ec51c18fb79a3663f79f6534
SonTrungTo <son.trung.to@gmail.com>
2020-07-25 17:53:05 +0300

    grep first version, it didnt work!

--- EloquentJS/ch20/grep.js
+++ EloquentJS/ch20/grep.js
@@ -1,1 +1,6 @@
 const {grep} = require("./src/grepFunction");
+
+let regexArg = process.argv[2];
+let fileArgs = Array.from(process.argv).slice(3);
+
+console.log(grep(regexArg, fileArgs));
--- EloquentJS/ch20/src/grepFunction.js
+++ EloquentJS/ch20/src/grepFunction.js
@@ -1,7 +1,14 @@
 exports.grep = grep;
 
-const {readFile} = require("fs").promises;
+const {readFile} = require("fs");
 
-function grep(search, file) {
+function grep(pattern, files) {
   let match;
+
+  for (let file of files) {
+    readFile(String(file), "utf8", (err, text) => {
+      if (err) throw err;
+      if (match = pattern.exec(text)) return file;
+    });
+  }
 }
