5d47917fb256fcb5d164c196a19e4bba31965532
SonTrungTo <son.trung.to@gmail.com>
2020-07-31 03:25:34 +0300

    express, ch2 finished!

--- Express/ch2/ch2.js
+++ Express/ch2/ch2.js
@@ -1,9 +1,23 @@
-const {readFile} = require("fs").promises;
+// const {readFile} = require("fs").promises;
+//
+// readFile("myfile.txt", "utf8").then((text, err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(text.match(/x/gi).length + " Xs in the file.");
+// });
+const {createServer} = require("http");
 
-readFile("myfile.txt", "utf8").then((text, err) => {
-  if (err) {
-    console.log(err);
-    return;
+function handlerRequest(request, response) {
+  if (request.url == "/") {
+    response.end("Hello world!");
+  } else if (request.url == "/about") {
+    response.end("This is about page!");
+  } else {
+    response.end("OOPS!");
   }
-  console.log(text.match(/x/gi).length + " Xs in the file.");
-});
+  console.log("There is a request to: ", request.url);
+}
+
+createServer(handlerRequest).listen(3000);
