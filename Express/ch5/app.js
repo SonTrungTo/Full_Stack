a9f8e02f73a51b641454627a53c2f1da0e5a124f
SonTrungTo <son.trung.to@gmail.com>
2020-08-04 02:29:53 +0300

    weather app finished! better solutions exist, though.

--- Express/ch5/app.js
+++ Express/ch5/app.js
@@ -1,38 +1,45 @@
 const express   = require("express");
 const {resolve} = require("path");
-const zipdb = require("zippity-do-dah");
-const ForecastIo = require("forecastio");
+const bodyParser = require("body-parser");
+const request   = require("request");
 
 const app = express();
-const weather = new ForecastIo("");
+const apiKey = "c63a8baf76fdc40e38124f6e28ef6752";
 
 app.use(express.static(resolve(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("views", resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {weather:null, error: null});
 });
 
-app.get(/^\/(\d{5})$/, (req, res, next) => {
-  let zipInput  = req.params[0];
-  let {zipcode, latitude, longitude} = zipdb.zipcode(zipInput);
-  if (!zipcode) {
-    next();
-    return;
-  }
-
-  weather.forecast(latitude, longitude, (err, data) => {
+app.post("/", (req, res, next) => {
+  let city = req.body.city;
+  let url  = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  request(url, (err, response, body) => {
     if (err) {
-      next();
-      return ;
+      res.render("index", {weather: null, error: "OOPS! Something is wrong!"});
+      return;
     }
-
-    res.json({
-      zipcode,
-      temperature: data.currently.temperature
-    });
+    let forecast = JSON.parse(body);
+    if (forecast.main == undefined) {
+      res.render("index", {weather: null, error: "OOPS! Wrong input!"});
+      return;
+    }
+    let weather = {
+      city: forecast.name,
+      temperature: forecast.main.temp,
+      country: forecast.sys.country,
+      situation: forecast.weather[0].description,
+      icon: forecast.weather.icon,
+      humidity: forecast.main.humidity
+    };
+    let textWeather = `We are in ${weather.city}, ${weather.country}. It is
+now ${weather.temperature} degree Celsius, ${weather.situation} and the humidity is ${weather.humidity}.`;
+    res.render("index", {weather: textWeather, error: null});
   });
 });
 
--- Express/ch5/public/main.js
+++ Express/ch5/public/main.js
@@ -1,15 +1,14 @@
-let form = document.querySelector("form");
-let header = document.querySelector("h1");
-
-form.addEventListener("submit", event => {
-  event.preventDefault();
-
-  let zipInput = event.target.elements.zip.value.trim();
-  header.textContent = "Loading...";
-
-  fetch("/" + zipInput, {"Content-Type": "application/json"}).then(response => {
-    let data = JSON.parse(response);
-    let temperature = data.temperature;
-    header.textContent = `It is ${temperature} &#176; in ${zipInput}.`;
-  });
-});
+// let form = document.querySelector("form");
+// let header = document.querySelector("h1");
+//
+// form.addEventListener("submit", event => {
+//   event.preventDefault();
+//
+//   let city = event.target.elements.city.value.trim();
+//
+//   fetch("/", {header: {"Content-Type": "application/json"}}).then(res => res.text())
+//   .then(res => {
+//
+//     header.textContent = `${res}`;
+//   });
+// });
