4b0a25d796cd003d0fc1190e86e538889b44eca7
SonTrungTo <son.trung.to@gmail.com>
2020-08-08 00:49:22 +0300

    tutorial postgresql finished!
    REST architecture is finished,
    applied with standard CRUD HTTP methods

--- Database/postgresql/tutorial/app.js
+++ Database/postgresql/tutorial/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {urlencoded, json} = require("body-parser");
+const db = require("./queries");
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,12 @@ app.use(urlencoded({
 app.get("/", (request, response) => {
   response.json({info: 'Node.js, Express and Postgres.'});
 });
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
   console.log(`App running on ${port}.`);
