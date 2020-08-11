304be363d3fd756aea19dfca2e3c1532a060b119
SonTrungTo <son.trung.to@gmail.com>
2020-08-11 08:15:22 +0300

    routes.js finished!
    - added vars for templates usage.
    - added query for listing users.

--- Express/ch8/routes.js
+++ Express/ch8/routes.js
@@ -4,8 +4,20 @@ const User = require("./models/user");
 
 const router = express.Router();
 
-router.use((req, res, next) => {
+router.use((req, res, next) => { // these are for ejs templates
   res.locals.currentUser = req.user;
-  res.locals.errors;
-  res.locals.infos;
+  res.locals.errors = req.flash("error");
+  res.locals.infos = req.flash("info");
+  next();
 });
+
+router.get("/", (req, res, next) => { // Using queries to list users from the newest
+  User.find()
+  .sort({createdAt: "descending"})
+  .exec((err, users) => {
+    if (err) {return  next(err);}
+    res.render("index", {users});
+  });
+});
+
+module.exports = router;
