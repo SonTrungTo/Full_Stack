e005eef574534a66610ad9115153900d27ecb4ed
SonTrungTo <son.trung.to@gmail.com>
2020-08-12 21:39:25 +0300

    adding passport authentication...

--- Express/ch8/app.js
+++ Express/ch8/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const flash = require("connect-flash");
+const bodyParser = require("body-parser"); // parses HTML forms
+const cookieParser = require("cookie-parser"); // handles parsing of cookies and is required for user sessions
+const session = require("express-session"); // stores user sessions across different browsers
+const flash = require("connect-flash"); // displays error messages
+const passport = require("passport");  // {initialize, session} to initialize passport module and handle session
+const setUpPassport = require("./setuppassport");
 
 let salt1 = bcrypt.genSaltSync();
 let salt2 = bcrypt.genSaltSync();
@@ -16,6 +18,7 @@ const routes = require("./routes");
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/learn_about_me_db", {useNewUrlParser: true, useUnifiedTopology: true});
+setUpPassport();
 
 app.set("port", process.env.PORT || 3000);
 
@@ -31,6 +34,8 @@ app.use(session({
 }));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(routes);
 
 app.listen(app.get("port"), () => {
