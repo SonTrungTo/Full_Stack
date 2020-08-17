934aaafe2f57ddee7d72650bf53c00410c0d1cf5
SonTrungTo <son.trung.to@gmail.com>
2020-08-17 21:53:07 +0300

    testing User Agent string being sent back as plain/text

---
+++ Express/ch9_SuperTest/test/txt.js
@@ -0,0 +1,13 @@
+const app = require("../app");
+
+describe("plain text response", () => {
+
+  it("returns a plain text response", (done) => {
+    // ...
+  });
+
+  it("returns your User Agent", (done) => {
+    // ...
+  });
+
+});
