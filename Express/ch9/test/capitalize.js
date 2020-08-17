91fc26fa58f93c3e4dd51f7645ab6e424d2cac7a
SonTrungTo <son.trung.to@gmail.com>
2020-08-17 11:45:50 +0300

    my first Mocha/Chai test

--- Express/ch9/test/capitalize.js
+++ Express/ch9/test/capitalize.js
@@ -1,1 +1,13 @@
-const capitalize;
+const capitalize = require("../capitalize");
+
+const chai   = require("chai");
+const expect = chai.expect;
+
+describe("capitalize", () => {
+
+  it("capitalizes single words", () => {
+    expect(capitalize("express")).to.equal("Express");
+    expect(capitalize("cats")).to.equal("Cats");
+  });
+
+});
