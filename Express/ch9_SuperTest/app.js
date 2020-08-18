0bb231d15fe3dc72e94614567e3297aea0521a53
SonTrungTo <son.trung.to@gmail.com>
2020-08-18 23:30:02 +0300

    plain text response passed!

---
+++ Express/ch9_SuperTest/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+  res.type("text");
+  res.send(req.headers["user-agent"]);
+});
+
+app.listen(app.get("port"), () => {
+  console.log("App started on port " + app.get("port"));
+});
+
+module.exports = app;
--- Express/ch9_SuperTest/test/txt.js
+++ Express/ch9_SuperTest/test/txt.js
@@ -3,24 +3,26 @@ const supertest = require("supertest");
 
 describe("plain text response", () => {
 
+  let request;
+  beforeEach( () => {
+    request = supertest(app)
+                .get("/")
+                .set("User-Agent", "my cool browser")
+                .set("Accept", "text/plain");
+  });
+
   it("returns a plain text response", (done) => {
-    supertest(app)
-        .get("/")
-        .set("User-Agent", "my cool browser")
-        .set("Accept", "text/plain")
+        request
         .expect("Content-Type", /text\/plain/)
         .expect(200)
         .end(done);
   });
 
   it("returns your User Agent", (done) => {
-    supertest(app)
-        .get("/")
-        .set("User-Agent", "my cool browser")
-        .set("Accept", "text/plain")
+        request
         .expect(res => {
           if (res.text !== "my cool browser") {
-            
+            throw new Error("Response does not contain User Agent");
           }
         })
         .end(done);
