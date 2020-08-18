4727a48fa5421b028adf658ba164efd47801d885
SonTrungTo <son.trung.to@gmail.com>
2020-08-18 15:11:37 +0300

    added tests for: plain text return, adding
    User-Agent

--- Express/ch9_SuperTest/test/txt.js
+++ Express/ch9_SuperTest/test/txt.js
@@ -1,13 +1,29 @@
 const app = require("../app");
+const supertest = require("supertest");
 
 describe("plain text response", () => {
 
   it("returns a plain text response", (done) => {
-    // ...
+    supertest(app)
+        .get("/")
+        .set("User-Agent", "my cool browser")
+        .set("Accept", "text/plain")
+        .expect("Content-Type", /text\/plain/)
+        .expect(200)
+        .end(done);
   });
 
   it("returns your User Agent", (done) => {
-    // ...
+    supertest(app)
+        .get("/")
+        .set("User-Agent", "my cool browser")
+        .set("Accept", "text/plain")
+        .expect(res => {
+          if (res.text !== "my cool browser") {
+            
+          }
+        })
+        .end(done);
   });
 
 });
