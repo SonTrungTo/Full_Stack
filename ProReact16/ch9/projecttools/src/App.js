74cac4f91c8170d8f8e8815ff7faafedf99eba2e
SonTrungTo <son.trung.to@gmail.com>
2020-08-19 21:59:43 +0300

    added how App.css + App.js -> static/bundle.js
    for different, bigger file (.svg),
    it becomes img tag

--- ProReact16/ch9/projecttools/src/App.js
+++ ProReact16/ch9/projecttools/src/App.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
+import "./App.css";
+import reactLogo from "./logo.svg";
+
+let   name = "Adam";
+const city = "London";
 
 export default class extends Component {
+
+  message = () => `Hello ${name} from ${city}`;
+
   render = () =>
+      <div className="text-center">
         <h4 className="bg-primary text-white text-center p-3">
-          This is an HTML element
+          {this.message()}
         </h4>
+        <img src={reactLogo} alt="reactLogo" />
+      </div>
 }
