359a4cd2a486b0f4acabcd0bc7931763fea10713
SonTrungTo <son.trung.to@gmail.com>
2020-08-25 23:35:04 +0300

    custom variable added to event handler.

--- ProReact16/ch12/reactevents/src/App.js
+++ ProReact16/ch12/reactevents/src/App.js
@@ -11,11 +11,11 @@ export default class App extends Component {
     /* this.handleEvent = this.handleEvent.bind(this); */
   }
 
-  handleEvent = (event) => {
+  handleEvent = (event, newTheme) => {
     event.persist();
     this.setState({
       counter: this.state.counter + 1,
-      theme: event.target.innerText === "Normal" ? "primary" : "danger"
+      theme: newTheme
     },
       () => this.setState({message: `${event.type}: ${this.state.counter}`}));
   };
@@ -32,9 +32,9 @@ export default class App extends Component {
         </div>
         <div className="text-center">
           <button className="btn btn-primary"
-          onClick={this.handleEvent}>Normal</button>
+          onClick={(e) => this.handleEvent(e, "primary")}>Normal</button>
           <button className="btn btn-danger m-1"
-          onClick={this.handleEvent}>Danger</button>
+          onClick={(e) => this.handleEvent(e, "danger")}>Danger</button>
         </div>
       </div>
     );
