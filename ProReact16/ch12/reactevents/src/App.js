943b76c2a413152e0f14232fc1b059038056f859
SonTrungTo <son.trung.to@gmail.com>
2020-08-25 17:25:14 +0300

    different approaches to accessing components features with events

--- ProReact16/ch12/reactevents/src/App.js
+++ ProReact16/ch12/reactevents/src/App.js
@@ -6,5 +6,28 @@ export default class App extends Component {
     this.state = {
       message: "Ready"
     };
+    /* this.handleEvent = this.handleEvent.bind(this); */
+  }
+
+  handleEvent = () => {
+    this.setState({message: "Clicked"});
+  };
+
+  /* handleEvent() {
+    this.setState({message: "Clicked"});
+  } */
+
+  render() {
+    return(
+      <div className="m-2">
+        <div className="h4 bg-primary text-white text-center p-2">
+          {this.state.message}
+        </div>
+        <div className="text-center">
+          <button className="btn btn-primary"
+          onClick={this.handleEvent}>Click Me</button>
+        </div>
+      </div>
+    );
   }
 }
