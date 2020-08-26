2db4c2fea999b2ac004fdd47120a8f891eb1b32b
SonTrungTo <son.trung.to@gmail.com>
2020-08-26 05:07:31 +0300

    Event handling finished!

--- ProReact16/ch12/reactevents/src/ThemeButton.js
+++ ProReact16/ch12/reactevents/src/ThemeButton.js
@@ -2,17 +2,30 @@ import React, {Component} from "react";
 
 export class ThemeButton extends Component {
 
-    handleClick = (event) => {
+    handleClick = (event, capturePhase = false) => {
         console.log(`ThemeButton: Type: ${event.type} `
         + `Bubble? ${event.bubbles} `
         + `Target: ${event.target.tagName} `
         + `CurrentTarget: ${event.currentTarget.tagName}`);
-        this.props.callback(this.props.theme);
+        if (capturePhase) {
+            if (this.props.theme === "danger") {
+                event.stopPropagation();
+                console.log("Event stopped!");
+            } else {
+                console.log("Skipped function prop: capture phase");
+            }
+        } else if (event.bubbles && event.currentTarget !== event.target) {
+            console.log("Skipped function prop: bubble phase");
+        } else {
+            console.log("Invoked function prop");
+            this.props.callback(this.props.theme);
+        }
     };
 
     render() {
         return(
-            <span className="m-1" onClick={this.handleClick}>
+            <span className="m-1" onClick={this.handleClick} 
+            onClickCapture={(e) => this.handleClick(e, true)} >
                 <button className={`btn btn-${this.props.theme}`}
                 onClick={this.handleClick}>
                     Select {this.props.theme} Theme
