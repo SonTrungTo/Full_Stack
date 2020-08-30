ad8e1ee6e667bb681dcaf503ec10e5873da59835
SonTrungTo <son.trung.to@gmail.com>
2020-08-30 23:48:01 +0300

    creating a specialized component:
    GeneralList <- SortedList

---
+++ ProReact16/ch14/composition/src/SortedList.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { GeneralList } from "./GeneralList";
+import { ActionButton } from "./ActionButton";
+
+export class SortedList extends Component {
+    
+}
