c0e20707c08b38dfc6a8c1dcf00163dbb949bf02
SonTrungTo <son.trung.to@gmail.com>
2020-09-04 13:59:01 +0300

    Modified: App, Display, Editor.
    Prepared to ch15

---
+++ ProReact16/ch15/forms/src/Display.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export class Display extends Component {
+    formatValue = (data) => Array.isArray(data) ?
+        data.join(", ") : data.toString();
+    
+    render() {
+        let keys = Object.keys(this.props.data);
+        if (keys.length === 0) {
+            return (
+                <div className="h5 bg-secondary text-white p-2">
+                    No Data
+                </div>
+            );
+        } else {
+            return (
+                <div className="container-fluid bg-secondary p-2">
+                    {keys.map(key => 
+                        <div key={key} className="h5 row text-white">
+                            <div className="col">{key}: </div>
+                            <div className="col">
+                                {this.formatValue(this.props.data[key])}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            );
+        }
+    }
+}
