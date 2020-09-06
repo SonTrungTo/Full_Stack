de346e6fba14a34eb3b11a92733bc2e6596bf993
SonTrungTo <son.trung.to@gmail.com>
2020-09-06 23:18:43 +0300

    adding FormValidator

--- ProReact16/ch15/forms/src/Editor.js
+++ ProReact16/ch15/forms/src/Editor.js
@@ -59,75 +59,79 @@ export class Editor extends Component {
     render() {
         return (
             <div className="h5 bg-info text-white p-2">
-                <div className="form-group">
-                    <label>Name</label>
-                    <input className="form-control"
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.updateFormValue} />
-                </div>
-                <div className="form-group">
-                    <label>Ice Cream Flavors</label>
-                    <select className="form-control"
-                    name="flavor" value={this.state.flavor}
-                    onChange={this.updateFormValue}>
-                        { this.flavors.map(flavor => 
-                            <option key={flavor} value={flavor}>
-                                {flavor}
-                            </option>
-                        ) }
-                    </select>
-                </div>
-                <div className="form-group">
-                    <label>Ice Cream Topping</label>
-                    <select className="form-control"
-                    name="topping" value={this.state.topping} multiple={true}
-                    onChange={this.updateFormValueOptions}>
-                        {this.toppings.map(topping =>
-                            <option key={topping} value={topping}>
-                                {topping}
-                            </option>
-                        )}
-                    </select>
-                </div>
-                <div className="form-group">
-                    <label>Favorite Book</label>
-                    {this.books.map(book => 
-                        <div className="form-check" key={book}>
-                            <input type="radio" className="form-check-input"
-                            name="book" value={book}
-                            checked={this.state.book === book}
-                            onChange={this.updateFormValue} />
-                            <label className="form-check-label">{book}</label>
-                        </div>
-                    )}
-                </div>
-                <div className="form-group">
-                    <div className="form-check">
-                        <input type="checkbox" className="form-check-input"
-                        name="nerd" checked={this.state.nerd}
-                        onChange={this.updateFormValueCheck} />
-                        <label className="form-check-label">I am a nerd!</label>
+                <FormValidator data={this.state} rules={this.rules}
+                submit={this.props.submit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input className="form-control"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.updateFormValue} />
+                        <ValidationMessage field="name" />
                     </div>
-                </div>
-                <div className="form-group">
-                    <label>Your advisor(s)</label>
-                    {this.advisors.map(adv =>
-                        <div className="form-check" key={adv}>
+                    <div className="form-group">
+                        <label>Ice Cream Flavors</label>
+                        <select className="form-control"
+                        name="flavor" value={this.state.flavor}
+                        onChange={this.updateFormValue}>
+                            { this.flavors.map(flavor => 
+                                <option key={flavor} value={flavor}>
+                                    {flavor}
+                                </option>
+                            ) }
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Ice Cream Topping</label>
+                        <select className="form-control"
+                        name="topping" value={this.state.topping} multiple={true}
+                        onChange={this.updateFormValueOptions}>
+                            {this.toppings.map(topping =>
+                                <option key={topping} value={topping}>
+                                    {topping}
+                                </option>
+                            )}
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Favorite Book</label>
+                        {this.books.map(book => 
+                            <div className="form-check" key={book}>
+                                <input type="radio" className="form-check-input"
+                                name="book" value={book}
+                                checked={this.state.book === book}
+                                onChange={this.updateFormValue} />
+                                <label className="form-check-label">{book}</label>
+                            </div>
+                        )}
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check">
                             <input type="checkbox" className="form-check-input"
-                            name={adv}
-                            checked={this.state.advisor.indexOf(adv) > -1}
-                            onChange={this.updateFormValueCheckPopulate} />
-                            <label className="form-check-label">{adv}</label>
+                            name="nerd" checked={this.state.nerd}
+                            onChange={this.updateFormValueCheck} />
+                            <label className="form-check-label">I am a nerd!</label>
                         </div>
-                    )}
-                </div>
-                <div className="form-group">
-                    <label>Order</label>
-                    <textarea className="form-control" name="order"
-                    value={this.state.order}
-                    onChange={this.updateFormValue} />
-                </div>
+                    </div>
+                    <div className="form-group">
+                        <label>Your advisor(s)</label>
+                        {this.advisors.map(adv =>
+                            <div className="form-check" key={adv}>
+                                <input type="checkbox" className="form-check-input"
+                                name={adv}
+                                checked={this.state.advisor.indexOf(adv) > -1}
+                                onChange={this.updateFormValueCheckPopulate} />
+                                <label className="form-check-label">{adv}</label>
+                            </div>
+                        )}
+                    </div>
+                    <div className="form-group">
+                        <label>Order</label>
+                        <textarea className="form-control" name="order"
+                        value={this.state.order}
+                        onChange={this.updateFormValue} />
+                    </div>
+                </FormValidator>
             </div>
         );
     }
