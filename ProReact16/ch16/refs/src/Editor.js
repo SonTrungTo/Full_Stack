7159965cef8500fe6b8806e5cc13b0551fc5dabd
SonTrungTo <son.trung.to@gmail.com>
2020-09-10 02:48:08 +0300

    an alternate way: uncontrolled form using ref

--- ProReact16/ch16/refs/src/Editor.js
+++ ProReact16/ch16/refs/src/Editor.js
@@ -4,26 +4,36 @@ export class Editor extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            name: "",
-            category: "",
-            price: ""
-        };
+        // this.state = {
+        //     name: "",
+        //     category: "",
+        //     price: ""
+        // };
         this.nameRef = React.createRef();
+        this.categoryRef = React.createRef();
+        this.priceRef = React.createRef();
     }
 
-    handleChange = (event) => {
-        event.persist();
-        this.setState(state => state[event.target.name] = event.target.value);
-    };
+    // handleChange = (event) => {
+    //     event.persist();
+    //     this.setState(state => state[event.target.name] = event.target.value);
+    // };
 
     handleAdd = () => {
-        this.props.callback(this.state);
-        this.setState({
-            name: "",
-            category: "",
-            price: ""
-        }, () => this.nameRef.current.focus() );
+        this.props.callback({
+            name: this.nameRef.current.value,
+            category: this.categoryRef.current.value,
+            price: this.priceRef.current.value
+        });
+        this.nameRef.current.value = "";
+        this.categoryRef.current.value = "";
+        this.priceRef.current.value = "";
+        this.nameRef.current.focus();
+        // this.setState({
+        //     name: "",
+        //     category: "",
+        //     price: ""
+        // }, () => this.nameRef.current.focus() );
     };
 
     render() {
@@ -31,18 +41,17 @@ export class Editor extends Component {
             <div className="form-group p-2">
                 <label>Name</label>
                 <input className="form-control" name="name"
-                value={this.state.name} onChange={this.handleChange}
-                autoFocus={true} ref={this.nameRef} />
+                autoFocus={true} ref={this.nameRef} defaultValue="Son To" />
             </div>
             <div className="form-group p-2">
                 <label>Category</label>
                 <input className="form-control" name="category"
-                value={this.state.category} onChange={this.handleChange} />
+                ref={this.categoryRef} />
             </div>
             <div className="form-group p-2">
                 <label>Price</label>
                 <input className="form-control" name="price"
-                value={this.state.price} onChange={this.handleChange} />
+                ref={this.priceRef} />
             </div>
             <div className="text-center">
                 <button className="btn btn-primary" onClick={this.handleAdd}>
