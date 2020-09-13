076c2d28cc94bbd90600caed338f44dbdefcbeb7
SonTrungTo <son.trung.to@gmail.com>
2020-09-14 00:38:45 +0300

    Unit test...ch17 done! Part II finished!
    - event simulation tested!
    - child props interacted with the component tested!

--- ProReact16/ch17/testapp/src/appBehavior.test.js
+++ ProReact16/ch17/testapp/src/appBehavior.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Adapter from "enzyme-adapter-react-16";
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {shallow, mount} from "enzyme";
 import App from "./App";
+import { ValueInput } from "./ValueInput";
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -27,4 +28,31 @@ it('update states data', () => {
 
     expect(wrapper.state("total"))
     .toBe(values.reduce((total, val) => total += val, 0));
+});
+
+it('update state data with a button click', () => {
+    const wrapper = shallow(<App />);
+    const button = wrapper.find("button").first();
+    const values = [10, 20, 30];
+
+    values.forEach((val, index) => {
+        wrapper.instance().updateFieldValue(index + 1, val);
+    });
+
+    button.simulate("click");
+
+    expect(wrapper.state("total"))
+    .toBe(values.reduce((total, val) => total += val, 0));
+});
+
+it('interaction between component and its child', () => {
+    const wrapper = mount(<App />);
+    const firstVal  = wrapper.find(ValueInput).first();
+    const inputElem = wrapper.find("input").first();
+
+    inputElem.simulate("change", {target: {value: "100"}});
+    wrapper.instance().updateTotal();
+
+    expect(firstVal.state("fieldValue")).toBe("100");
+    expect(wrapper.state("total")).toBe(100);
 });
