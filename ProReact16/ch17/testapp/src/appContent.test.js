85ba03d54c78dc75a3fbb3306ff0c7a35d53bbae
SonTrungTo <son.trung.to@gmail.com>
2020-09-13 20:47:47 +0300

    unit test: full-render with mount

--- ProReact16/ch17/testapp/src/appContent.test.js
+++ ProReact16/ch17/testapp/src/appContent.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {shallow, mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import App from "./App";
 import { ValueInput } from "./ValueInput";
@@ -10,4 +10,16 @@ it('Producing 3 ValueInputs', () => {
     const wrapper = shallow(<App />);
     const valCount = wrapper.find(ValueInput).length;
     expect(valCount).toBe(3);
+});
+
+it('Fully renders three inputs', () => {
+    const wrapper = mount(<App title="Test three inputs" />);
+    const count = wrapper.find("input.form-control").length;
+    expect(count).toBe(3);
+});
+
+it('Shallow renders zero inputs', () => {
+    const wrapper  = shallow(<App />);
+    const valCount = wrapper.find("input.form-control").length;
+    expect(valCount).toBe(0);
 });
