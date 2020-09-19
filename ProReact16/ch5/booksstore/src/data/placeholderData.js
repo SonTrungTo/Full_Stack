7cb4109942f60ad2a019bc4e8729bda37f9f0aa7
SonTrungTo <son.trung.to@gmail.com>
2020-09-20 02:58:10 +0300

    having added action/actioncreators/reducer/datatypes
    for redux and data interactions with the store data.
    This is the basic for foundations in Redux!

---
+++ ProReact16/ch5/booksstore/src/data/placeholderData.js
@@ -0,0 +1,45 @@
+export const data = {
+    categories: ["Economics", "Mathematics", "Computer Science", "Psychology"],
+    products: [
+        {
+            id: 1,
+            name: "P1",
+            category: "Economics",
+            author: "MWG",
+            description: "P1 (Microeconomics)",
+            price: 3
+        },
+        {
+            id: 2,
+            name: "P2",
+            category: "Economics",
+            author: "Wooldridge",
+            description: "P2 (Econometrics)",
+            price: 4
+        },
+        {
+            id: 3,
+            name: "P3",
+            category: "Mathematics",
+            author: "Apostol",
+            description: "P3 (Calculus)",
+            price: 5
+        },
+        {
+            id: 4,
+            name: "P4",
+            category: "Computer Science",
+            author: "CLRS",
+            description: "P4 (Algorithm)",
+            price: 6
+        },
+        {
+            id: 5,
+            name: "P5",
+            category: "Psychology",
+            author: "Rumelhart",
+            description: "P5 (Cognitive Science)",
+            price: 7
+        }
+    ]
+};
