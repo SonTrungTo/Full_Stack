2a83d34157bfc013baea10b9b54784774ba6d08c
SonTrungTo <son.trung.to@gmail.com>
2020-09-21 02:07:27 +0300

    ToggleLink added and improved categories menu!

--- ProReact16/ch5/booksstore/src/shop/CategoryNavigation.js
+++ ProReact16/ch5/booksstore/src/shop/CategoryNavigation.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
 
 export class CategoryNavigation extends Component {
     render() {
         return <React.Fragment>
-            <Link to={ this.props.baseUrl }
-            className="btn btn-secondary btn-block">All</Link>
+            <ToggleLink to={this.props.baseUrl} exact={true}>
+                All
+            </ToggleLink>
             {this.props.categories && this.props.categories.map(cat => 
-                <Link key={cat} className="btn btn-secondary btn-block"
+                <ToggleLink key={cat}
                 to={`${this.props.baseUrl}/${cat.toLowerCase()}`}>
                     {cat}
-                </Link>
+                </ToggleLink>
             )}
         </React.Fragment>
     }
