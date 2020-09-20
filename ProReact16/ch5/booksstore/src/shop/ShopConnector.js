b4e427e041c1d35131e1c211c8ee71f77cf8c475
SonTrungTo <son.trung.to@gmail.com>
2020-09-21 00:34:20 +0300

    ShopConnector written!
    using connect from react-redux to
    map storeData into props!

--- ProReact16/ch5/booksstore/src/shop/ShopConnector.js
+++ ProReact16/ch5/booksstore/src/shop/ShopConnector.js
@@ -17,3 +17,23 @@ const filterProducts = (products = [], category) =>
     (!category || category === "All")
     ? products :
     products.filter(p => p.category.toLowerCase() === category.toLowerCase());
+
+export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
+    class extends Component {
+        render() {
+            return <Switch>
+                <Route path="/shop/products/:category?"
+                render={(renderProps) => 
+                    <Shop {...this.props} {...renderProps}
+                    products={filterProducts(this.props.products,
+                    renderProps.match.params.category)} />} />
+                <Redirect to="/shop/products" />
+            </Switch>
+        }
+
+        componentDidMount() {
+            this.props.loadData(DataTypes.CATEGORIES);
+            this.props.loadData(DataTypes.PRODUCTS);       
+        }
+    }
+)
