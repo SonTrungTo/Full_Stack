3b91bdac362b7ad7ac059ec550d687ded7deeb89
SonTrungTo <son.trung.to@gmail.com>
2020-09-22 18:36:39 +0300

    CartDetails written. Now writing CartDetailsRows.
    It is for details of the shopping cart
    and proceed to checkout!

---
+++ ProReact16/ch5/booksstore/src/shop/CartDetailsRows.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class CartDetailsRows extends Component {
+
+    handleChange = (product, event) => {
+        this.props.updateCartQuantity(product, event.target.value);
+    };
+
+    render() {
+        if (condition) {
+            
+        } else {
+            
+        }
+    }
+};
