ac4c9e35307cf80f83da8b161bedd6b2df283715
SonTrungTo <son.trung.to@gmail.com>
2020-09-22 19:48:14 +0300

    CartDetailsRows.js written!

--- ProReact16/ch5/booksstore/src/shop/CartDetailsRows.js
+++ ProReact16/ch5/booksstore/src/shop/CartDetailsRows.js
@@ -7,10 +7,34 @@ export class CartDetailsRows extends Component {
     };
 
     render() {
-        if (condition) {
-            
+        if (!this.props.cart || this.props.cart.length === 0) {
+            return <tr>
+                <td colSpan="5">Your cart is empty</td>
+            </tr>
         } else {
-            
+            return <React.Fragment>
+                {this.props.cart.map(item => 
+                    <tr key={item}>
+                        <td>
+                            <input type="number" value={item.quantity}
+                            onChange={(ev) => this.handleChange(item.product, ev)} />
+                        </td>
+                        <td>{item.product.name}</td>
+                        <td><span>&#8364;</span>{item.product.price.toFixed(2)}</td>
+                        <td><span>&#8364;</span>{(item.product.price * item.quantity).toFixed(2)}</td>
+                        <td>
+                            <button className="btn btn-danger btn-sm"
+                            onClick={() => this.props.removeFromCart(item.product)}>
+                                Remove
+                            </button>
+                        </td>
+                    </tr>
+                )}
+                <tr>
+                    <th colSpan="3" className="text-right">Total:</th>
+                    <th colSpan="2"><span>&#8364;</span>{this.props.cartPrice.toFixed(2)}</th>
+                </tr>
+            </React.Fragment>
         }
     }
 };
