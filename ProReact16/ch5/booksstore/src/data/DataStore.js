c6393eab69194ce98c5c696b74983fe07ddadc44
SonTrungTo <son.trung.to@gmail.com>
2020-09-23 19:45:36 +0300

    changed DataStore and ActionCreators...
    so that they will process action objects
    before it is received by redux data store,
    using applyMiddleware from redux!

--- ProReact16/ch5/booksstore/src/data/DataStore.js
+++ ProReact16/ch5/booksstore/src/data/DataStore.js
@@ -1,7 +1,9 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { ShopReducer } from "./ShopReducer";
 import { CartReducer } from "./CartReducer";
 import { CommonReducer } from "./CommonReducer";
+import { asyncActions } from "./AsyncMiddleware";
 
 export const BooksStoreDataStore
-= createStore(CommonReducer(ShopReducer, CartReducer));
+= createStore(CommonReducer(ShopReducer, CartReducer),
+applyMiddleware(asyncActions));
