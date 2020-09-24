c3d8d3be01f89e4beffbf99e86cbbc6250a45127
SonTrungTo <son.trung.to@gmail.com>
2020-09-24 17:53:10 +0300

    DataGetter.js for handling product data, ongoing...

---
+++ ProReact16/ch5/booksstore/src/data/DataGetter.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { DataTypes } from "./Types";
+
+export class DataGetter extends Component {
+
+    render() {
+        return <React.Fragment>{this.props.children}</React.Fragment>
+    }
+
+    componentDidMount = () => this.getData();
+    componentDidUpdate = () => this.getData();
+
+    getData = () => {
+        const dsData = this.props.products_params || {};
+        const rtData = {
+            _page: this.props.match.params.page || 1,
+            category_like: (this.props.match.params.category || "") === "all" 
+                ? "" : this.props.match.params.category,
+            _sort: this.props.sortKey || "name",
+            _limit: this.props.pageSize || 5
+        };
+
+        if (condition) {
+            
+        }
+    }
+}
