e747bebf185e49ffec0c97a92d63366ef401115f
SonTrungTo <son.trung.to@gmail.com>
2020-09-25 17:36:42 +0300

    PaginationControls, phase 2...
    Added new types, actions, and updated reducer.
    Now writing PaginationControls to display it in HTML

---
+++ ProReact16/ch5/booksstore/src/PaginationControls.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { PaginationButtons } from "./PaginationButtons";
+
+export class PaginationControls extends Component {
+    constructor(props) {
+        super(props);
+        this.pageSizes;
+        this.sortKeys;
+    }
+}
