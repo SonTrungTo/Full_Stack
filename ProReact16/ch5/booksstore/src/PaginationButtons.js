e817c5d57503d12d9c765fd04d57f3e728a66f43
SonTrungTo <son.trung.to@gmail.com>
2020-09-26 13:23:20 +0300

    PaginationButtons finished!

--- ProReact16/ch5/booksstore/src/PaginationButtons.js
+++ ProReact16/ch5/booksstore/src/PaginationButtons.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export class PaginationButtons extends Component {
 
     getPageNumbers = () => {
-        if (this.props.pageCount < 4) {
+        if (this.props.pageCount <= 4) {
             return [...Array(this.props.pageCount + 1).keys()].slice(1);
         } else if (this.props.currentPage <= 4) {
             return [1, 2, 3, 4, 5];
@@ -35,11 +35,36 @@ export class PaginationButtons extends Component {
                     <span className="h4">...</span>
                 </React.Fragment>
             }
+
             {
                 this.getPageNumbers().map(num =>
-                    
+                    <button className={`btn mx-1 ${num === current
+                    ? btn-primary : btn-secondary}`}
+                    onClick={() => navigate(num)} 
+                    key={num}>
+                        {num}
+                    </button>
                 )
+            
             }
+
+            {
+                current <= (pageCount - 4) &&
+                <React.Fragment>
+                    <span className="h4">...</span>
+                    <button 
+                    className="btn btn-secondary mx-1"
+                    onClick={() => navigate(pageCount)}>
+                        {pageCount}
+                    </button>
+                </React.Fragment>
+            }
+
+            <button className="btn btn-secondary mx-1"
+            disabled={current === pageCount}
+            onClick={() => navigate(current + 1)}>
+                Next
+            </button>
         </React.Fragment>
     }
 }
