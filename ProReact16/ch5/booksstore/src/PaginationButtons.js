3bd846c10b2bf51d5f02b205218c16385f19a6a0
SonTrungTo <son.trung.to@gmail.com>
2020-09-26 04:21:50 +0300

    PaginationControls.js written! Struggling
    with displaying PaginationButtons.js, i.e,
    how to navigating through the pages!

---
+++ ProReact16/ch5/booksstore/src/PaginationButtons.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class PaginationButtons extends Component {
+
+    getPageNumbers = () => {
+        if (condition) {
+            
+        } else if (condition) {
+            
+        }
+    };
+}
--- ProReact16/ch5/booksstore/src/PaginationControls.js
+++ ProReact16/ch5/booksstore/src/PaginationControls.js
@@ -4,7 +4,46 @@ import { PaginationButtons } from "./PaginationButtons";
 export class PaginationControls extends Component {
     constructor(props) {
         super(props);
-        this.pageSizes;
-        this.sortKeys;
+        this.pageSizes = this.props.sizes || [5, 10, 25, 100];
+        this.sortKeys = this.props.keys || ["Name", "Price"];
+    }
+
+    handlePageSizeChange = (ev) => {
+        this.props.setPageSize(ev.target.value);
+    };
+
+    handleSortPropertyChange = (ev) => {
+        this.props.setSortProperty(ev.target.value);
+    };
+
+    render() {
+        return <div className="m-2">
+            <div className="text-center m-1">
+                <PaginationButtons currentPage={this.props.currentPage}
+                    pageCount={this.props.pageCount}
+                    navigate={this.props.navigateToPage} />
+            </div>
+            <div className="form-inline justify-content-center">
+                <select className="form-control"
+                onChange={ this.handlePageSizeChange }
+                value={ this.props.pageSize || this.pageSizes[0] }>
+                    { this.pageSizes.map(s =>
+                        <option value={s} key={s}>
+                            {s} results per page
+                        </option>
+                    )}
+                </select>
+                <select className="form-control"
+                onChange={ this.handleSortPropertyChange }
+                value={ this.props.sortKey || this.sortKeys[0] }>
+                    { this.sortKeys.map(k =>
+                        <option value={k.toLowerCase()}
+                        key={k}>
+                            Sort by {k.toLowerCase()}
+                        </option>
+                    )}
+                </select>
+            </div>
+        </div>
     }
 }
