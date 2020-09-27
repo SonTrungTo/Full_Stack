798efbd5ff84bce7da5e88347abf4b0babe135b2
SonTrungTo <son.trung.to@gmail.com>
2020-09-27 03:44:33 +0300

    We are now writing ValidatedForm...

---
+++ ProReact16/ch5/booksstore/src/forms/ValidatedForm.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { ValidationError } from "./ValidationError";
+import { GetMessages } from "./ValidationMessages";
+
+export class ValidatedForm extends Component {
+    
+}
