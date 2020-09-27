3e7a7218dbbf3f91e5db99709ecc211c5a64e28f
SonTrungTo <son.trung.to@gmail.com>
2020-09-28 01:22:34 +0300

    ValidationMessages.js finished!

--- ProReact16/ch5/booksstore/src/forms/ValidationMessages.js
+++ ProReact16/ch5/booksstore/src/forms/ValidationMessages.js
@@ -1,8 +1,9 @@
 export function GetMessages(element) {
     const messages = [];
     if (element.validity.valueMissing) {
-        
+        messages.push("Required field!");
     } else if (element.validity.typeMismatch) {
-        
+        messages.push(`Invalid type: ${element.type}`);
     }
+    return messages;
 }
