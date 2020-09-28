2bf1bdc8ddeeece1d2393e0e78965b93593a8fc6
SonTrungTo <son.trung.to@gmail.com>
2020-09-28 16:47:54 +0300

    Thanks component completed! Adding it to shopConnector!

--- ProReact16/ch5/booksstore/src/shop/Thanks.js
+++ ProReact16/ch5/booksstore/src/shop/Thanks.js
@@ -4,7 +4,20 @@ import { Link } from "react-router-dom";
 export class Thanks extends Component {
     render() {
         return <div className="container-fluid">
-            
+            <div className="col bg-dark text-white">
+                <div className="navbar-brand">
+                    BOOKS STORE
+                </div>
+            </div>
+            <div className="text-center m-2">
+                <h2>Thank you!</h2>
+                <p>Your order, #{this.props.order ? this.props.order.id : 0},
+                is received!</p>
+                <p>We will ship your goods as soon as possible.</p>
+                <Link to="/shop" className="btn btn-primary">
+                    Return To Store
+                </Link>
+            </div>            
         </div>
     }
 }
