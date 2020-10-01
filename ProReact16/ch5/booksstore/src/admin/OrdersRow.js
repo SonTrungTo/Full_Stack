87fd708db3631b6f299eb072dcf459eb2b83542b
SonTrungTo <son.trung.to@gmail.com>
2020-10-01 17:32:44 +0300

    OrdersRow written. Defining Connector Component!

---
+++ ProReact16/ch5/booksstore/src/admin/OrdersRow.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export class OrdersRow extends Component {
+
+    calcTotal = (products) => products.reduce((total, p) =>
+        total += p.quantity * p.product.price , 0);
+    
+    getShipping = (order) => order.shipped ?
+        <i className="fa fa-shipping-fast text-success" /> :
+        <i className="fa fa-exclamation-circle text-danger" />;
+    
+    render = () =>
+        <tr>
+            <td>{ this.props.order.id }</td>
+            <td>{ this.props.order.name }</td>
+            <td>{ this.props.order.email }</td>
+            <td className="text-right">
+                <span>&euro;</span>{ this.calcTotal(this.props.order.products) }
+            </td>
+            <td className="text-center">
+                <button className="btn btn-sm btn-block bg-muted">
+                    { this.getShipping(this.props.order) }
+                    <span>
+                        {
+                            this.props.order.shipped ? "Shipped"
+                            : "Pending"
+                        }
+                    </span>
+                </button>
+            </td>
+        </tr>
+}
