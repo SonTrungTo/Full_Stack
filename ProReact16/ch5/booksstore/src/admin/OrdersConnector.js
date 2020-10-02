a1769948099c28b382b3872f918783327385274a
SonTrungTo <son.trung.to@gmail.com>
2020-10-02 22:34:48 +0300

    OrdersConnector finished!

--- ProReact16/ch5/booksstore/src/admin/OrdersConnector.js
+++ ProReact16/ch5/booksstore/src/admin/OrdersConnector.js
@@ -2,4 +2,19 @@ import { graphql } from "react-apollo";
 import { ordersSummaryQuery } from "./clientQueries";
 import { OrdersTable } from "./OrdersTable";
 
-const vars = { onlyShipped: false, };
+const vars = { onlyShipped: false, page: 1, pageSize: 10, sort: "id" };
+
+export const OrdersConnector = graphql(ordersSummaryQuery, {
+    options: (props) => ({variables: vars}),
+    props: ({data: {loading, orders, refetch}}) => ({
+        totalSize: loading ? 0 : orders.totalSize,
+        currentPage: vars.page,
+        pageSize: vars.pageSize,
+        orders: loading ? [] : orders.orders,
+        pageCount: loading ? 0 : Math.ceil(orders.totalSize / vars.pageSize),
+        navigateToPage: (page) => {vars.page = Number(page); refetch(vars)},
+        sortKey: vars.sort,
+        setPageSize: (size) => {vars.pageSize = Number(size); refetch(vars)},
+        setSortProperty: (key) => {vars.sort = key; refetch(vars)}
+    })
+})(OrdersTable);
