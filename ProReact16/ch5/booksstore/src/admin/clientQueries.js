bdcbe0ef64049fcb60617b717f622332dc3f3144
SonTrungTo <son.trung.to@gmail.com>
2020-10-02 23:29:18 +0300

    sucessfully displayed admin page!

--- ProReact16/ch5/booksstore/src/admin/clientQueries.js
+++ ProReact16/ch5/booksstore/src/admin/clientQueries.js
@@ -1,8 +1,8 @@
-import { gql } from "graphql-tag";
+import gql from "graphql-tag";
 
 export const ordersSummaryQuery = gql`
     query($onlyShipped: Boolean, $page: Int, $pageSize: Int, $sort: String) {
-        orders(onlyUnshipped: $$onlyShipped) {
+        orders(onlyUnshipped: $onlyShipped) {
             totalSize,
             orders(sort: $sort, page: $page, pageSize: $pageSize) {
                 id, name, email, shipped,
