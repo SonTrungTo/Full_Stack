38e690a2ec009e6db740032f1cec39e195ec7a6f
SonTrungTo <son.trung.to@gmail.com>
2020-10-12 12:32:42 +0300

    Written codes for bundling client.
    3. Serving static files, /dist
    4. Adding HTTP req and server response to render template.

--- MERNFourProjects/simpleSetUp/server/server.js
+++ MERNFourProjects/simpleSetUp/server/server.js
@@ -1,5 +1,20 @@
 import express from "express";
 import devBundle from "./devBundle";
+import path from "path";
+import template from "../template";
 
 const app = express();
+const CURRENT_WORKING_DIR = process.pwd();
+const port = process.env.PORT || 3000;
+
 devBundle.compile(app);
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+app.get("/", (req, res) => {
+    res.status(200).send(template());
+});
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(`App is running at ${port}`);
+});
