2e103e08fdecc3b1e394f207cf43a7e31d91ab75
SonTrungTo <son.trung.to@gmail.com>
2020-10-12 05:55:34 +0300

    configurations for production similar to client prod,
    since these will not need hot-loading modules and
    debugging plugins. Next is configuring Nodemon
    for monitoring codes changes in these bundles.

---
+++ MERNFourProjects/simpleSetUp/webpack.config.client.production.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+const CURRENT_WORKING_DIR = process.pwd();
+
+const config = {
+    mode: "production",
+    entry: [
+        path.join(CURRENT_WORKING_DIR, 'client/main.js')
+    ],
+    output: {
+        path: path.join(CURRENT_WORKING_DIR, '/dist'),
+        fileName: "bundle.js",
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    }
+};
+
+module.exports = config;
