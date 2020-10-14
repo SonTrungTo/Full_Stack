29b25c6b9d3b9c845618bd976d49ac88c51f2b24
SonTrungTo <son.trung.to@gmail.com>
2020-10-15 00:42:40 +0300

    webpack configurations for backend...

---
+++ MERNFourProjects/mern_skeleton/webpack.config.server.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const webpack = require("webpack");
+const CURRENT_WORKING_DIR = process.cwd();
+const nodeExternals = require("webpack-node-externals");
+
+const config = {
+    name: "server",
+    entry: []
+};
+
+module.exports = config;
