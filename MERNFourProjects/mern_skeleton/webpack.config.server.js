dea9b001387780cef2c3d7dd88b76ee8e8483b6d
SonTrungTo <son.trung.to@gmail.com>
2020-10-15 04:13:05 +0300

    added configurated versions.

--- MERNFourProjects/mern_skeleton/webpack.config.server.js
+++ MERNFourProjects/mern_skeleton/webpack.config.server.js
@@ -5,7 +5,26 @@ const nodeExternals = require("webpack-node-externals");
 
 const config = {
     name: "server",
-    entry: []
+    entry: [
+        path.join(CURRENT_WORKING_DIR, 'server/server.js')
+    ],
+    target: "node",
+    output: {
+        path: path.join(CURRENT_WORKING_DIR, 'dist/'),
+        filename: "server.generated.js",
+        publicPath: '/dist/',
+        libraryTarget: 'commonjs2'
+    },
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    }
 };
 
 module.exports = config;
